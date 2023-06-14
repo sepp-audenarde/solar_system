621a19c6b44d0c8fabeeab45a1a5708eccd8df59
sepp-audenarde <dmitriy.kr999@gmail.com>
2023-06-14 12:15:07 +0300

    deploy

--- src/components/Loader.jsx
+++
@@ -1,6 +0,0 @@
-import { Html } from '@react-three/drei'
-import { SolarSystemLoading } from 'react-loadingg/lib/BabelLoading';
-
-export default function Loader() {
-  return <Html center style={{zIndex:'500', paddingRight: '54px'}}><SolarSystemLoading color='white' size='large'/></Html>
-}
--- src/context/valtio.jsx
+++
@@ -1,9 +0,0 @@
-import { proxy } from "valtio";
-
-const state = proxy({
-	firstScreen: true,
-	secondScreen: false,
-	activePlanetIdx: 2,
-});
-
-export { state };
